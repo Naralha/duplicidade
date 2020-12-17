69e46903b6473736b1ba3684e0272b436995f344
Luis Nascimento <luispaulo.nascimento@outlook.com>
2020-12-17 16:03:12 -0300

    ajuste de projeto

--- duplicidade/src/main/java/br/com/duplicidade/obra/Obra.java
+++ duplicidade/src/main/java/br/com/duplicidade/obra/Obra.java
@@ -1,7 +1,6 @@
 package br.com.duplicidade.obra;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Column;
--- duplicidade/src/main/java/br/com/duplicidade/swagger/SwaggerConfig.java
+++ duplicidade/src/main/java/br/com/duplicidade/swagger/SwaggerConfig.java
@@ -1,24 +1,30 @@
 package br.com.duplicidade.swagger;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
-	@Bean
+/*	@Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
           .select()
           .apis(RequestHandlerSelectors.any())
           .paths(PathSelectors.any())
-          .build();
+          .build()
+          .useDefaultResponseMessages(false)
+          .globalResponseMessage(RequestMethod.GET, responseMessageForGET());
     }
+	
+	private List<ResponseMessage> responseMessageForGET()
+	{
+	    return new ArrayList<ResponseMessage>() {{
+	        add(new ResponseMessageBuilder()
+	            .code(500)
+	            .message("500 message")
+	            .responseModel(new ModelRef("Error"))
+	            .build());
+	        add(new ResponseMessageBuilder()
+	            .code(403)
+	            .message("Forbidden!")
+	            .build());
+	    }};
+	}*/
 }
